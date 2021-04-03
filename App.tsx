92dd479bd27ba2dd860fbf337e85c634b0787b81
Bruno Batalha <brunobatalha37@gmail.com>
2021-04-03 18:46:43 -0400

    uses new design system

--- App.tsx
+++ App.tsx
@@ -1,25 +1,55 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Dimensions, AsyncStorage, TouchableOpacity, ScrollView, Clipboard, ToastAndroid, Alert, BackHandler } from 'react-native';
-import {Feather, MaterialIcons} from '@expo/vector-icons';
+import {
+  StyleSheet, AsyncStorage, Clipboard, ToastAndroid,
+  Alert, BackHandler, ScrollView, StatusBar,
+} from 'react-native';
+import * as eva from '@eva-design/eva';
+import {
+  ApplicationProvider, Text, Layout, List,
+  Button, Icon, IconRegistry, OverflowMenu,
+  MenuItem, Input
+} from '@ui-kitten/components';
+import { default as theme } from './custom-theme.json';
+import { EvaIconsPack } from '@ui-kitten/eva-icons'; // https://akveo.github.io/eva-icons/#/
+
+
+const TEXTS_KEY = 'fields';
+
+interface Field {
+  text: string,
+  visibleMenu: boolean
+}
+
+function usePrevious(value: any): any {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  },[value]);
+  return ref.current;
+}
+
 export default function App() {
 
-  const scrollFields = useRef< null | HTMLElement>(null);
-  const [fields, setFields]:any = useState<String[]>([]);
+  const scrollFields = useRef<null | HTMLElement>(null);
+  const [fields, setFields] = useState<Field[]>([]);
+  const prevLength = usePrevious(fields.length)
 
   useEffect(() => {
-    loadFields();
+    getFields().then(setFields);
   }, []);
 
   useEffect(() => {
-    if (scrollFields.current) {
-      scrollFields.current.scrollToEnd({animated: true});
+    if(fields.length > prevLength){
+      setTimeout(() => {
+        scrollFields.current.scrollToEnd({ animated: true });        
+      }, 100);
     }
-  }, [fields]);
+  }, [fields.length]);
 
   useEffect(() => {
     // componentDidMount    
     const backAction = () => {
-      showAlert('Até depois :D','Deseja realmente sair? Não esqueça de salvar suas alterações',()=>{BackHandler.exitApp()})
+      showAlert('Até depois :D', 'Deseja realmente sair? Não esqueça de salvar suas alterações', () => { BackHandler.exitApp() })
       return true;
     };
 
@@ -30,16 +60,17 @@ export default function App() {
   }, []);
 
 
-  async function saveFields(fields: any) {
+  async function saveFields(fields: Field[]) {
     try {
-      await AsyncStorage.setItem('fields', fields.join('SEPARATOR'));
+      const texts = fields.map((field: Field) => field.text);
+      await AsyncStorage.setItem(TEXTS_KEY, texts.join('SEPARATOR'));
       showToast('Salvo');
     } catch (err) {
-      alert('Erro: '+err);
+      alert('Erro: ' + err);
     }
   }
 
-  function showToast(text:string){
+  function showToast(text: string) {
     ToastAndroid.showWithGravityAndOffset(
       text,
       ToastAndroid.SHORT,
@@ -49,44 +80,51 @@ export default function App() {
     );
   }
 
-  async function loadFields() {
+  async function getFields(): Promise<Field[]> {
     try {
-      const data:string | null = await AsyncStorage.getItem('fields')
-      setFields(data ? data.split('SEPARATOR') : []);
+      const data: string | null = await AsyncStorage.getItem(TEXTS_KEY);
+      if (!data) return [];
+
+      const texts = data.split('SEPARATOR');
+      const fields = texts.map(text => ({ text, visibleMenu: false }));
+
+      return fields || [];
     } catch (err) {
       alert('Erro em recuperar nota anterior' + err)
+      return [];
     }
   }
 
-  function addField(){
-    setFields([...fields, '']);
+  function addField() {
+    setFields([...fields, { text: '', visibleMenu: false }]);
   }
 
-  function handleChangeText(text:string, index:number){
-    const newFields = [...fields];
-    newFields[index] = text;
-    setFields(newFields);
+  function onChangeText(text: string, index: number) {
+    setFields([
+      ...fields.slice(0, (index + 1) - 1),
+      { ...fields[index], text: text },
+      ...fields.slice((index + 1))])
   }
-  
-  function deleteField(index:number){
+
+  function deleteField(index: number) {
     showAlert(
       'Cuidado!',
       "Deseja realmente excluir este campo? Esta ação não pode ser desfeita",
-      ()=>{
-        const newFields = [...fields.filter((field:any,idx:number)=> idx != index)];
-        setFields(newFields);
-      });    
+      () => {
+        const updatedFields: Field[] = [...fields.filter((field: Field, idx: number) => idx != index)];
+        setFields(updatedFields);
+      });
   }
 
-  function showAlert(title:string, description:string, cbConfirm,  cbCancel=()=>{}){
+  function showAlert(title: string, description: string, cbConfirm, cbCancel = () => { }) {
     Alert.alert(title, description,
       [
         {
           text: "Cancelar",
           onPress: () => cbCancel(),
         },
-        { 
-          text: "Continuar", 
+        {
+          text: "Continuar",
           onPress: () => cbConfirm()
         }
       ],
@@ -94,120 +132,136 @@ export default function App() {
     )
   }
 
-  function copyField(text:string)  {
+  function copyField(text: string) {
     Clipboard.setString(text);
     showToast('Copiado');
   }
 
+  const CopyIcon = (props) => (
+    <Icon {...props} name='copy' />
+  );
+  const DeleteIcon = (props) => (
+    <Icon {...props} name='trash-2' />
+  );
+  const MenuFieldIcon = (props) => (
+    <Icon {...props} name='more-vertical-outline' />
+  );
+  const SaveIcon = (props) => (
+    <Icon {...props} name='save' />
+  );
+  const PlusIcon = (props) => (
+    <Icon {...props} name='plus' />
+  );
+
+  function renderToggleButton(index: number) {
+    return (
+      <Button
+        status='basic'
+        appearance='ghost'
+        onPress={() => setVisibleMenuItem(index, true)}
+        accessoryLeft={MenuFieldIcon}
+      />
+    )
+  }
+
+  function setVisibleMenuItem(index: number, visibleMenu: boolean) {
+    setFields([
+      ...fields.slice(0, (index + 1) - 1),
+      { ...fields[index], visibleMenu },
+      ...fields.slice((index + 1))])
+  }
+
+  function MenuField({ text, visibleMenu, index }) {
+    return (
+      <OverflowMenu
+        visible={visibleMenu}
+        anchor={() => renderToggleButton(index)}
+        onBackdropPress={() => { setVisibleMenuItem(index, false) }}
+      >
+        <MenuItem title="Copiar" accessoryLeft={CopyIcon} onPress={() => copyField(text)} />
+        <MenuItem title="Excluir" accessoryLeft={DeleteIcon} onPress={() => deleteField(index)} />
+      </OverflowMenu>
+    )
+  }
+
+  function renderItem({ item, index }) {
+    return (
+      <Layout style={styles.containerField}>
+        <Input
+          keyboardAppearance='dark'
+          style={styles.fieldText}
+          multiline
+          onChangeText={text => onChangeText(text, index)}
+          value={item.text}
+          placeholder='Ex.: Re:zero, Jujutsu Kaisen...'
+        />
+        <MenuField
+          text={item.text}
+          visibleMenu={item.visibleMenu}
+          index={index}
+        />
+      </Layout>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Nota Rápida</Text>
+    <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
+      <IconRegistry icons={EvaIconsPack} />
+      <StatusBar hidden={true} />
 
-      <ScrollView style={{height: '100%', marginBottom: 40}} ref={scrollFields}>
-          {fields.map((field:any,index:number)=>
-              <View style={styles.containerField} key={index}>                
-                <TextInput
-                  multiline={true}
-                  style={styles.fieldText}
-                  value={field}
-                  onChangeText={text=>handleChangeText(text,index)}
-                  placeholder='Ex.: Assistir 2° temporada do anime X' >
-                </TextInput>        
-                <TouchableOpacity  style={styles.buttonIcon} onPress={e=>copyField(field)}>
-                  <Feather name='copy' size={20} color='#353839' />
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.buttonIcon}  onPress={e=>deleteField(index)}>
-                  <MaterialIcons name='delete' size={21} color='red' />
-                </TouchableOpacity>
-              </View>        
-          )}
-      </ScrollView>
-        
-      <View style={styles.containerButtons}>
-        <TouchableOpacity style={{...styles.buttonSave, ...styles.centerContent}} 
-        onPress={e=>saveFields(fields)}>
-          {/* <Text style={styles.buttonSaveText}>Salvar
-          </Text> */}
-            <Feather name='save' size={20} color='#fff' />
-        </TouchableOpacity>
+      <Layout style={styles.container}>
+        <Text style={styles.title}>Nota Rápida</Text>
+        {/* <Layout style={{ flex: 1 }}> */}
+        {/* <ScrollView> */}
+        <List
+          ref={scrollFields}
+          style={{ backgroundColor: 'transparent' }}
+          data={fields}
+          renderItem={renderItem}
+        />
+        {/* </ScrollView> */}
+        {/* </Layout> */}
+        <Layout style={styles.containerButtons}>
+          <Button style={{ ...styles.button, ...styles.centerContent }}
+            onPress={e => saveFields(fields)}
+            status='success'
+            accessoryLeft={SaveIcon} />
 
-        <TouchableOpacity style={{...styles.buttonAdd, ...styles.centerContent}} 
-          onPress={e=>addField()}>
-          {/* <Text style={styles.buttonSaveText}>Novo
-          </Text> */}
-            <Feather name='plus' size={20} color='#fff' />
-          
-        </TouchableOpacity>
-      </View>
-    </View>
+          <Button style={{ ...styles.button, ...styles.centerContent }}
+            onPress={e => addField()}
+            accessoryLeft={PlusIcon} />
+        </Layout>
+
+      </Layout>
+    </ApplicationProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: '5%',
+    padding: 8,
     flex: 1,
-    backgroundColor: '#fff',
   },
   title: {
-    fontSize: 25,
-    marginTop: 25,
-    borderBottomColor: '#c3c3c3',
-    borderBottomWidth: 2
-  },
-  buttonIcon:{
-    flex: 1,
+    height: 56,
+    fontSize: 24,
+    textAlignVertical: "center",
   },
   fieldText: {
     flex: 10,
-    fontSize: 16,
-    borderBottomColor: '#5CB7F4',
-    borderBottomWidth: 2,
-    marginRight: 5
   },
-  buttonAdd: {
-    backgroundColor: '#000',
-    flex:1
+  button: {
+    flex: 1
   },
-  buttonDelete: {
-    backgroundColor: '#000',
-    height: 30,
-    width: '10%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  buttonSave: {    
-    backgroundColor: '#008000',
-    flex:1
-  },
-  buttonDeleteText: {
-    color: 'red',
-    fontSize: 45,
-    textTransform: 'uppercase',
-    letterSpacing: 2,
-    fontWeight: "900"
-  },
-  buttonSaveText: {
-    color: '#fff',
-    fontSize: 20,
-    textTransform: 'uppercase',
-    letterSpacing: 2,
-  },
-  containerField:{
-    display: 'flex',
+  containerField: {
     flexDirection: 'row',
-    marginTop: 15,
-    padding: 5
   },
-  containerButtons:{
-    position: 'absolute',
-    bottom: 0,
+  containerButtons: {
     height: 50,
-    width: Dimensions.get('window').width,
+    width: '100%',
     flexDirection: 'row',
   },
-  centerContent:{
+  centerContent: {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center'
